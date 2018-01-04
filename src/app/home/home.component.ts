393c9bcfe0863fd4dc2d916494cd72542d3e118d
samilda <salem-mohri@hotmail.fr>
2018-01-04 17:16:10 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private _data: DataService) { }
+
+  ngOnInit() {
+  }
+
+}
